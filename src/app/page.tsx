640793425e2b619a64bc483abdde316e5dbbd56e
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-09-23 19:15:59 +0900

    fix: Biomeによるlintの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 export default function Home() {
   //わざと型エラーを出すために、string型をnumber型に代入する
   const ciTest: string = "CIをわざと失敗させるためのテスト"
-  return(
-    <div>
-      テスト
-    </div>
-  )
+  return <div>{ciTest}</div>
 }
